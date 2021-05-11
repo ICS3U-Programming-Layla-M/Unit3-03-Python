7c001e1d3ff871eaf58f2e53c418a8417d731791
Ubuntu <ubuntu@ip-172-31-57-171.ec2.internal>
2021-05-11 17:01:52 +0000

    added rand_num_guess.py

---
+++ rand_num_guess.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Layla Michel
+# Created on: May, 11, 2021
+# This program asks the user to input a number between 0 and 9,
+# generates a random number and displays a message depending
+# on if the guess is the same as the random number or not
+
+import random
+
+# get the user guess
+number = int(input("Guess what number I am thinking of between 0 and 9: "))
+
+
+def main():
+    # define variable
+    correct_guess = random.randint(1, 9)
+
+    # check if guess is correct and display message
+    # depending on if it's right or wrong
+    if (number == correct_guess):
+        print("You guessed correctly!")
+    else:
+        print("You guessed wrong. The correct answer was: {}\
+        ". format(correct_guess))
+
+
+if __name__ == "__main__":
+    main()
